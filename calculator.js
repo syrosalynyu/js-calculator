96c1bf69ce918ee4c6d9a842c5fa88d408186f1b
Rosalyn <syrosalynyu@gmail.com>
2020-03-31 21:17:37 -0700

    Create input validations inside of main function
    validation 1.) Object can't be empty
    validation 2.) when input(s) is/are not number(s)
    validation 3.) when denominator is 0
    validation 4.) the valuse of 'operation' can only be one of the supported operators

--- calculator.js
+++ calculator.js
@@ -1,5 +1,43 @@
 const exampleAdditionInput = {
-  num1: 3,
+  num1: -3,
   num2: 5,
   operation: 'add',
+};
+
+const calculate = function(input) {
+  // when Object is empty
+  if (Object.keys(input).length === 0) {
+    console.log('Your input is empty! Please try again.');
+    return false;
+  }
+
+  // validate num1 and num2. Both need to be numbers
+  if (typeof input.num1 !== 'number' || typeof input.num2 !== 'number' ) {
+    console.log('Please provide valid number(s)!');
+    return false;
+  }
+
+  // denominator can't be 0
+  if (input.num2 === 0) {
+    console.log('Denominator can\'t be 0!');
+    return false
+  }
+
+  //validate the operation. Must be one of the following
+  if (input.operation !== 'add' &&
+      input.operation !== '+' &&
+      input.operation !== 'subtract' &&
+      input.operation !== '-' &&
+      input.operation !== 'multiply' &&
+      input.operation !== '*' &&
+      input.operation !== 'divide' &&
+      input.operation !== '/'
+  ) { 
+    console.log('Please provide valid operator!');
+    return false;
+  }
+
 }
+
+
+calculate(exampleAdditionInput);
